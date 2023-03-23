077ff1c29acc4382a4a4f176f1590cc7de09c465
cledson <cledsonmendes10@gmail.com>
2023-03-23 18:16:19 -0300

    Cadastrando usuario pelo app

--- galeria/models.py
+++ galeria/models.py
@@ -1,11 +1,31 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIAS = [
+        ("NEBULOSA", "nebulosa"),
+        ("ESTRELA", "estrela"),
+        ("GALÁXIA", "galáxia"),
+        ("PLANETA", "planeta")
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="user"
+    )
     
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
-    
+        return self.nome
+        
